720aad08aa26a3e61654fa3e71f6f7ce6e877837
georggrubner <grubnergeorg@gmail.com>
2024-10-29 00:47:29 +0100

    move colors to Layout

--- src/Layout/Header/index.tsx
+++ src/Layout/Header/index.tsx
@@ -1,5 +1,5 @@
 import { useColorScheme } from '@mui/material/styles'
-import { Box, IconButton } from '@mui/material'
+import { Box, IconButton, Stack } from '@mui/material'
 import { DarkMode, LightMode } from '@mui/icons-material'
 import React from 'react'
 
@@ -13,24 +13,20 @@ export const Header = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    const nextTheme = mode === 'dark' ? 'light' : 'dark'
+
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                width: '100%',
-                alignItems: 'center',
-                justifyContent: 'center',
-                bgcolor: 'background.default',
-                color: 'text.primary',
-                height: 64,
-            }}
-        >
-            <IconButton
-                aria-label={mode === 'dark' ? 'light' : 'dark'}
-                onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
-            >
-                {mode === 'dark' ? <LightMode /> : <DarkMode />}
-            </IconButton>
-        </Box>
+        <Stack justifyContent="space-around" height={64} direction="row" alignItems="center">
+            <Box>left</Box>
+            <Stack direction="row">
+                <IconButton
+                    title={`Switch to ${nextTheme} theme`}
+                    aria-label={nextTheme}
+                    onClick={() => setMode(nextTheme)}
+                >
+                    {mode === 'dark' ? <LightMode /> : <DarkMode />}
+                </IconButton>
+            </Stack>
+        </Stack>
     )
 }
--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,11 +1,12 @@
+import { Stack } from '@mui/material'
 import { Body } from './Body'
 import { Header } from './Header'
 
 export const Layout = () => {
     return (
-        <>
+        <Stack bgcolor="background.default" color="text.primary">
             <Header />
             <Body />
-        </>
+        </Stack>
     )
 }
