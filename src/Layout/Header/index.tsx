0de23ef1b804989d89bdce01ba444235d2e0fabb
georggrubner <grubnergeorg@gmail.com>
2024-10-29 00:26:17 +0100

    move theme switching to header

--- src/Layout/Header/index.tsx
+++ src/Layout/Header/index.tsx
@@ -1,3 +1,24 @@
+import { useColorScheme } from '@mui/material/styles'
+import { Box, Button } from '@mui/material'
+
 export const Header = () => {
-    return <>Header</>
+    const { mode, setMode } = useColorScheme()
+    if (!mode) {
+        return null
+    }
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                width: '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+                bgcolor: 'background.default',
+                color: 'text.primary',
+                height: 64,
+            }}
+        >
+            <Button onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}>switch</Button>
+        </Box>
+    )
 }
