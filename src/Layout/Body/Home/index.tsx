69782aee8ea16ba3a1aeaed7c86a628d80e4f0de
georggrubner <grubnergeorg@gmail.com>
2024-10-29 17:24:36 +0100

    split into Home and CV

---
+++ src/Layout/Body/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Stack, Typography } from '@mui/material'
+import { Profile } from './Profile'
+
+export const Home = () => (
+    <Stack direction="row" margin={10} justifyContent="center" gap={8}>
+        <Profile />
+        <Stack alignItems="flex-start" flexGrow={3} marginTop={4}>
+            <Typography variant="h4">Frontend-Developer</Typography>
+            <Typography variant="h5">Georg Grubner</Typography>
+            <Typography>I am a Frontend-Developer living in Vienna, Austria</Typography>
+        </Stack>
+    </Stack>
+)
--- src/Layout/Body/index.tsx
+++ src/Layout/Body/index.tsx
@@ -1,15 +1,11 @@
-import { Stack, Typography } from '@mui/material'
-import { Profile } from './Profile'
+import { CV } from './CV'
+import { Home } from './Home'
 
 export const Body = () => {
     return (
-        <Stack direction="row" margin={10} justifyContent="center" gap={8}>
-            <Profile />
-            <Stack alignItems="flex-start" flexGrow={3} marginTop={4}>
-                <Typography variant="h4">Frontend-Developer</Typography>
-                <Typography variant="h5">Georg Grubner</Typography>
-                <Typography>I am a Frontend-Developer living in Vienna, Austria</Typography>
-            </Stack>
-        </Stack>
+        <>
+            <Home />
+            <CV />
+        </>
     )
 }
