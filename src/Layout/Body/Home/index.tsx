78ebcdebfe6c0fcaa9d734b3f2a1591b5d8e14c2
georggrubner <grubnergeorg@gmail.com>
2024-10-29 17:32:46 +0100

    moved margin to Body container

--- src/Layout/Body/Home/index.tsx
+++ src/Layout/Body/Home/index.tsx
@@ -2,7 +2,7 @@ import { Stack, Typography } from '@mui/material'
 import { Profile } from './Profile'
 
 export const Home = () => (
-    <Stack direction="row" margin={10} justifyContent="center" gap={8}>
+    <Stack direction="row" gap={12}>
         <Profile />
         <Stack alignItems="flex-start" flexGrow={3} marginTop={4}>
             <Typography variant="h4">Frontend-Developer</Typography>
--- src/Layout/Body/index.tsx
+++ src/Layout/Body/index.tsx
@@ -1,8 +1,13 @@
+import { Stack } from '@mui/material'
 import { usePage } from '../../zustand'
 import { CV } from './CV'
 import { Home } from './Home'
 
 export const Body = () => {
     const { page } = usePage()
-    return <>{page === 'home' ? <Home /> : <CV />}</>
+    return (
+        <Stack margin={20} justifyContent="center">
+            {page === 'home' ? <Home /> : <CV />}
+        </Stack>
+    )
 }
