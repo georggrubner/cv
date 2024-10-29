01b528b644257d6ff3cab18c832d0cfa8c11f5db
georggrubner <grubnergeorg@gmail.com>
2024-10-29 17:28:34 +0100

    added page switching to header

--- src/Layout/Header/index.tsx
+++ src/Layout/Header/index.tsx
@@ -2,26 +2,33 @@ import { GitHub, Home, LinkedIn, Mail, Person } from '@mui/icons-material'
 import { IconButton, Stack } from '@mui/material'
 import { ThemeSwitcher } from './ThemeSwitcher'
 import { ReactNode } from 'react'
+import { usePage } from '../../zustand'
 
 const linksMap: Array<[ReactNode, string]> = [
-    [<Person />, ''],
     [<LinkedIn />, 'https://www.linkedin.com/in/georg-grubner-3547861a6/'],
     [<GitHub />, 'https://github.com/georggrubner'],
     [<Mail />, 'mailto:georg@grubner.tech'],
 ]
 
-export const Header = () => (
-    <Stack justifyContent="space-around" height={64} direction="row" alignItems="center">
-        <IconButton href="http://grubner.tech/">
-            <Home />
-        </IconButton>
-        <Stack direction="row">
-            {linksMap.map(([icon, href]) => (
-                <IconButton target="_blank" rel="noopener noreferrer" key={href} href={href}>
-                    {icon}
+export const Header = () => {
+    const { setPage } = usePage()
+
+    return (
+        <Stack justifyContent="space-around" height={64} direction="row" alignItems="center">
+            <IconButton onClick={() => setPage('home')}>
+                <Home />
+            </IconButton>
+            <Stack direction="row">
+                <IconButton onClick={() => setPage('cv')}>
+                    <Person />
                 </IconButton>
-            ))}
-            <ThemeSwitcher />
+                {linksMap.map(([icon, href]) => (
+                    <IconButton target="_blank" rel="noopener noreferrer" key={href} href={href}>
+                        {icon}
+                    </IconButton>
+                ))}
+                <ThemeSwitcher />
+            </Stack>
         </Stack>
-    </Stack>
-)
+    )
+}
