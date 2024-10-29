dbb3541e6bf79d683f71cc67bb3dbc67d750b454
georggrubner <grubnergeorg@gmail.com>
2024-10-29 19:36:27 +0100

    bootstrap cv page

--- src/Layout/Body/CV/index.tsx
+++ src/Layout/Body/CV/index.tsx
@@ -1,1 +1,19 @@
-export const CV = () => <>cv</>
+import { Stack, Typography } from '@mui/material'
+
+export const CV = () => {
+    return (
+        <Stack>
+            <Typography variant="h2">Georg Grubner</Typography>
+            <Stack>
+                <Typography variant="h4">About Me</Typography>
+                <Typography variant="body1">react</Typography>
+                <Typography variant="body1">node</Typography>
+                <Typography variant="body1">react-query</Typography>
+                <Typography variant="body1">zustand</Typography>
+                <Typography variant="body1">next</Typography>
+                <Typography variant="body1">mui</Typography>
+                <Typography variant="body1">ts</Typography>
+            </Stack>
+        </Stack>
+    )
+}
