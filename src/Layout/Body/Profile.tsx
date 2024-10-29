2dbf3f6447d4f6a9daa453c637e0ff09c1c11908
georggrubner <grubnergeorg@gmail.com>
2024-10-29 02:03:53 +0100

    added basic body component

---
+++ src/Layout/Body/Profile.tsx
@@ -0,0 +1,18 @@
+import { Box } from '@mui/material'
+import profile from '/public/profile.jpg'
+
+export const Profile = () => (
+    <Box display="flex" justifyContent="flex-end" flexGrow={2}>
+        <img
+            style={{
+                boxShadow: '0 0 10px',
+                borderRadius: 22,
+                objectFit: 'cover',
+            }}
+            src={profile}
+            width="300"
+            height="400"
+            alt="profile"
+        />
+    </Box>
+)
--- src/Layout/Body/index.tsx
+++ src/Layout/Body/index.tsx
@@ -1,3 +1,15 @@
+import { Stack, Typography } from '@mui/material'
+import { Profile } from './Profile'
+
 export const Body = () => {
-    return <>Body</>
+    return (
+        <Stack direction="row" margin={10} justifyContent="center" gap={8}>
+            <Profile />
+            <Stack alignItems="flex-start" flexGrow={3} marginTop={4}>
+                <Typography variant="h4">Frontend-Developer</Typography>
+                <Typography variant="h5">Georg Grubner</Typography>
+                <Typography variant="h6">I am a Frontend-Developer living in Vienna, Austria</Typography>
+            </Stack>
+        </Stack>
+    )
 }
