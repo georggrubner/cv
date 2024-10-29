05cf64e0702111886c1d26f4738933b4a80ed58a
georggrubner <grubnergeorg@gmail.com>
2024-10-29 17:21:03 +0100

    install zustand and create simple store

---
+++ src/zustand/index.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand'
+
+type Page = 'home' | 'cv'
+
+export type PageStore = {
+    page: Page
+    setPage: (page: Page) => void
+}
+
+export const usePage = create<PageStore>((set) => ({
+    page: 'home',
+    setPage: (page: Page) => set({ page }),
+}))
