b555a6c115b503fa97188b99e4e8c1e701e2e3f5
georggrubner <grubnergeorg@gmail.com>
2024-10-30 17:49:37 +0100

    flipped Projects and Skills

--- src/Layout/Body/CV/index.tsx
+++ src/Layout/Body/CV/index.tsx
@@ -5,12 +5,6 @@ export const CV = () => {
     return (
         <Box maxWidth="50vw" marginX="auto">
             <Stack justifyContent="space-around">
-                <Typography variant="h4">#Skills</Typography>
-                {skills.map(([name, href]) => (
-                    <Link width="fit-content" href={href} target="_blank" rel="noopener noreferrer">
-                        <Typography variant="body1">{name}</Typography>
-                    </Link>
-                ))}
                 <Typography variant="h4">#Projects</Typography>
                 {projects.map(({ name, description, features, tech, achievements }) => (
                     <Stack marginLeft={3}>
@@ -30,6 +24,12 @@ export const CV = () => {
                             </ListItem>
                         </List>
                     </Stack>
+                ))}
+                <Typography variant="h4">#Skills</Typography>
+                {skills.map(([name, href]) => (
+                    <Link width="fit-content" href={href} target="_blank" rel="noopener noreferrer">
+                        <Typography variant="body1">{name}</Typography>
+                    </Link>
                 ))}
             </Stack>
         </Box>
