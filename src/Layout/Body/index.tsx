e51a1fd987f73b939dd991b7271c43b5b0f57d8a
georggrubner <grubnergeorg@gmail.com>
2024-10-30 17:54:43 +0100

    added margin bottom

--- src/Layout/Body/index.tsx
+++ src/Layout/Body/index.tsx
@@ -5,5 +5,9 @@ import { Home } from './Home'
 
 export const Body = () => {
     const { page } = usePage()
-    return <Stack justifyContent="center">{page === 'home' ? <Home /> : <CV />}</Stack>
+    return (
+        <Stack marginBottom={5} justifyContent="center">
+            {page === 'home' ? <Home /> : <CV />}
+        </Stack>
+    )
 }
