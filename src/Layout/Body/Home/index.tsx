7ea4edd45d3287560f69aaac55bec1952a7caac0
georggrubner <grubnergeorg@gmail.com>
2024-10-30 16:39:54 +0100

    improved home sentence

--- src/Layout/Body/Home/index.tsx
+++ src/Layout/Body/Home/index.tsx
@@ -5,9 +5,14 @@ export const Home = () => (
     <Stack direction={{ xs: 'column', md: 'row' }} gap={{ xs: 5, md: 10 }} alignItems="center">
         <Profile />
         <Stack alignItems="flex-start" flexGrow={3} gap={2} marginX={2}>
-            <Typography variant="h4">Frontend-Developer</Typography>
-            <Typography variant="h5">Georg Grubner</Typography>
-            <Typography>I am a Frontend-Developer living in Vienna,&nbsp;Austria</Typography>
+            <Typography variant="h4">Georg Grubner</Typography>
+            <Typography variant="h5">Frontend-Developer living in Vienna,&nbsp;Austria</Typography>
+            <Typography maxWidth={400}>
+                Passionate and skilled frontend developer with a strong focus on creating accessible, high-performance
+                web applications that prioritize user experience and inclusivity. Known for a keen attention to detail
+                and the ability to optimize code for speed and responsiveness, delivering seamless, cross-platform
+                solutions.
+            </Typography>
         </Stack>
     </Stack>
 )
